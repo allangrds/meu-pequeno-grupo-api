24bd878b1595ec4858a07ffaf218b91d54df9efb
Allan Ramos <allangrds@gmail.com>
2020-05-31 17:51:25 -0300

    feat(sentry): add tool to capture api errors

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const helmet = require('helmet')
 const timeout = require('connect-timeout')
+const Sentry = require('@sentry/node')
 
 const routes = require('./routes')
 const errorHandler = require('./middlewares/errorHandler')
@@ -17,15 +18,24 @@ const port = process.env.API_PORT
 
 const limiter = rateLimit()
 
+Sentry.init({ dsn: process.env.SENTRY_DSN })
+
+app.use(Sentry.Handlers.requestHandler())
+app.use(Sentry.Handlers.errorHandler())
 app.use(timeout('8s'))
 app.set('trust proxy', 1)
+app.use(Sentry.Handlers.errorHandler())
 app.use(helmet())
 app.disable('x-powered-by')
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+app.use(Sentry.Handlers.errorHandler())
 app.use(limiter)
+app.use(Sentry.Handlers.errorHandler())
 app.use(haltOnTimedout)
+app.use(Sentry.Handlers.errorHandler())
 app.use(routes)
+app.use(Sentry.Handlers.errorHandler())
 app.use(haltOnTimedout)
 
 app.get('*', (req, res) => (
